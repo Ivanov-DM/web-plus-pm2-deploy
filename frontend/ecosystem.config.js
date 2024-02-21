5179d48d73a8e8ab835409773bcca125b9a58301
Dmitry Ivanov <di.ivanov2018@gmail.com>
2024-02-21 23:26:25 +0300

    add frontend deployment

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm ci && npm run build',
+    },
+  },
+};
